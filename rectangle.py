795c7c728bda02d4e422785ef5eb5f4b3346ab88
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-19 08:46:23 -0400

    Updated from Brython Server: 3/19/2018 8:46:23 AM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,9 @@
+#Daniel Bandler
+#3/19/18
+#rectangle.py - prints out perimeter and area of rectangle
 
+def rectangle(side1,side2):
+    print("the area is", side1*side2)
+    print("the perimeter is", 2*side1+2*side2)
+    
+rectangle(3,4)
