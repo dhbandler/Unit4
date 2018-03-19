dd45ea129aa9fd7d9896cb658bc37edc864f99d1
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-19 08:39:06 -0400

    Update functionDemo.py

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,31 @@
+#Daniel Bandler
+#3/19/18
+#functionDemo.py - how to write functions
+def hw():
+    print("Hello, world!")
+    
+def double(thingToDouble):
+    print(thingToDouble * 2)
+    
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+        
+def slope(x1, y1, x2, y2):
+    print((y2-y1)/(x2-x1))
+    
+#slope(1,1,2,2)
+#slope(True,True,False,False) #how?
+
+#bigger(3,4)
+#bigger(4,3)
+#bigger("Bandler, Daniel")
+#bigger(True,False)
+#double(12) #tests double function
+#double("w") #tests double function with string
+#double(False) #tests double with bool
+
+
 
