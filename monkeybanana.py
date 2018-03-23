4449e542ebb79e3b6dd258e682c5d5aa7d380ab5
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-23 08:28:33 -0400

    Updated from Brython Server: 3/23/2018 8:28:33 AM

--- monkeybanana.py
+++ monkeybanana.py
@@ -8,7 +8,7 @@ from random import randint
 #Constants
 
 
-ROWS = 18
+ROWS = 16
 COLS = 34
 CELL_SIZE = 30
 
@@ -35,13 +35,17 @@ def moveUp(event):
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            
+
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
+    updateScore()
+    
+def updateScore():    
     data["score"] += 1
-    print(data["score"])
-    Sprite(text)
+    data["scoreText"].destroy() #remove old writing
+    scoreBox = TextAsset("Score = "+str(data["score"]))
+    data["scoreText"] = Sprite(scoreBox,(0,ROWS*CELL_SIZE+4))
 
 if __name__== "__main__":
     
@@ -58,11 +62,13 @@ if __name__== "__main__":
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),yellow)
-    text = TextAsset("your score is", fill=black,style= "bold 40pt Georgia")
+    scoreBox = TextAsset("Score = 0")
+
     
     Sprite(jungleBox)
     monkey = Sprite(monkeyBox)
     banana = Sprite(bananaBox, (CELL_SIZE*COLS/2,CELL_SIZE*ROWS/2))
+    data["scoreText"] = Sprite(scoreBox, (0,ROWS*CELL_SIZE+4))
    
     App().listenKeyEvent("keydown", "right arrow", moveRight)
     App().listenKeyEvent("keydown", "down arrow", moveDown)
