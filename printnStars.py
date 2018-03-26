88968a8350023db1222b5d8b544d30aa7b22d732
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-26 08:37:26 -0400

    Updated from Brython Server: 3/26/2018 8:37:25 AM

--- printnStars.py
+++ printnStars.py
@@ -1,5 +1,10 @@
 #Daniel Bandler
 #3/19/18
 #printnStars.py - prints n layers of stars
+n = int(input("How many rows? "))
 
-def stars
+def stars(n):
+    for i in range(1,n+1):
+        print("*"*i)
+
+print(stars(n))
