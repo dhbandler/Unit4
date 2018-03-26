16bd0156cb787373b3a94e8d7ab7f62aab8ec332
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-26 09:21:04 -0400

    Updated from Brython Server: 3/26/2018 9:21:03 AM

--- vprint.py
+++ vprint.py
@@ -1,1 +1,10 @@
+#Daniel Bandler
+#3/26/18
+#vprint.py prints string vertically
 
+string = input("type something!")
+
+def vprint(string):
+    total = len(string)
+    for i in range(1,total):
+        
