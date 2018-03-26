2e1e9bee7d41bb554a00f2a4a6e486398ae481bd
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-26 08:29:24 -0400

    Updated from Brython Server: 3/26/2018 8:29:23 AM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,16 @@
+#Daniel Bandler
+#3/26/18
+#warmup11.py returns true if num is prime, otherwise false
+#ex. print(prime(9)) == false
+
+def prime(num):
+    for i in range(2,num):
+        if num%i == 0:
+            return False
+    return True   
+
+        
+print(prime(9))
+print(prime(10))
+print(prime(11))
 
