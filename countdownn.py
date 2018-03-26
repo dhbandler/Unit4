3f710a8ff7a4f1ceb2aefe2ebb213db3f7956bdb
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-26 08:45:19 -0400

    Updated from Brython Server: 3/26/2018 8:45:19 AM

--- countdownn.py
+++ countdownn.py
@@ -1,1 +1,13 @@
+#Daniel
+#3/26/18
+#countdownn.py counts down from integer
+n = int(input("Enter a number "))
 
+def countdown(n):
+    i = n
+    while i > 0:
+        print(i)
+        i -= 1
+        if i == 0:
+            print("BOOM")
+countdown(n)
