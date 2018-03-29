b15999ba6b7e67556af99e25a9caa43b5324d2a4
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-29 08:39:26 -0400

    Updated from Brython Server: 3/29/2018 8:39:25 AM

--- warmup12.py
+++ warmup12.py
@@ -3,12 +3,11 @@
 #warmup12.py -- finds GCF
 
 def gcf(x,y):
-    if x%y == 0:
-        return y
         
-    for i in range(int(x/2,1), 0, -1):
+    for i in range(x,0,-1):
         if x%i == 0 and y%i==0:
-            gcf = i
-            break
-    return i
-gcf(12,15)
+            return i
+
+print(gcf(12,15))
+print(gcf(5,9))
+print(gcf(16,64))
