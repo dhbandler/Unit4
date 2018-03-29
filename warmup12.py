534559324e4a80c30c50808833ea67b53adf2c36
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-29 08:27:11 -0400

    Updated from Brython Server: 3/29/2018 8:27:10 AM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,12 @@
+#Daniel Bandler
+#3/29/18
+#warmup12.py -- finds GCF
 
+def gcf(x,y):
+    if x%y == 0:
+        return y
+        
+    for i in range(1,x/2):
+        if x%i == 0 and y%i==0:
+            return i
+gcf(12,15)
