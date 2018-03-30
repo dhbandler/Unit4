37ac1772906df442c3d618e84f82eece264088dc
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-30 08:24:26 -0400

    Updated from Brython Server: 3/30/2018 8:24:25 AM

--- distance.py
+++ distance.py
@@ -1,1 +1,10 @@
+#Daniel Bandler
+#3/30/18
+#distance.py does distance formula
 
+def distance(x1,y1,x2,y2):
+   xval = (x2-x1) 
+   yval = (y2-y1)
+   print((yval**2+xval**2)**.5)
+   
+distance(3,4,-5,2)
